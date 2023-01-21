a3c1a5bd0529a0d9b5c591ea5ae0e755275de091
Srinath Sridhar <Sridhar.Srinath@in.bosch.com>
2023-01-21 23:50:20 +0530

    initial solution

---
+++ src/main/java/BucketSample/BucketSampleApplication.java
@@ -0,0 +1,68 @@
+package BucketSample;
+
+import java.util.*;
+
+public class BucketSampleApplication {
+
+    /*
+    Below method assumes single number as range string if startReading and endReading is same
+     */
+    private static String getBucketRange(int startReading, int endReading) {
+        String range;
+        if(startReading == endReading) {
+            range = String.valueOf(startReading);
+        }
+        else {
+            range = startReading + "-" + endReading;
+        }
+        return range;
+    }
+
+    private static boolean isAdjacentNumbersValid(int reading1, int reading2) {
+        return reading1 == reading2 || reading1 == reading2-1;
+    }
+
+    /*
+    Method to produce result in Map<String, Integer> format for given input integer array readings
+     */
+    public static Map<String, Integer> getReadingsRange(int[] readings) {
+        Map<String, Integer> resultMap = new LinkedHashMap<>();
+        if(readings.length >= 1) {
+            Arrays.sort(readings);
+            int startValue=readings[0], currValue=readings[0], readingCount=1;
+            for(int i=1; i<readings.length; i++) {
+                if(isAdjacentNumbersValid(currValue,readings[i])) {
+                    currValue = readings[i];
+                    readingCount += 1;
+                }
+                else {
+                    resultMap.put(getBucketRange(startValue,currValue), readingCount);
+                    startValue = readings[i];
+                    currValue = readings[i];
+                    readingCount = 1;
+                }
+            }
+            resultMap.put(getBucketRange(startValue,currValue), readingCount);
+        }
+        return resultMap;
+    }
+
+    /*
+        Method to produce result in below CSV format(String) for given input integer array readings.
+        Range, Readings\n
+     */
+    public static String getReadingsRangeCSV(int[] readings) {
+        Map<String, Integer> resultMap = getReadingsRange(readings);
+        String resultString;
+        if(resultMap.isEmpty()) {
+            resultString = "";
+        }
+        else {
+            resultString = "Range, Readings\n";
+            for(Map.Entry<String, Integer> entry: resultMap.entrySet()){
+                resultString += (entry.getKey() + ", " + entry.getValue() + "\n");
+            }
+        }
+        return resultString;
+    }
+}
---
+++ src/test/java/BucketSample/TestBucketSampleApplication.java
@@ -0,0 +1,75 @@
+package BucketSample;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class TestBucketSampleApplication {
+
+    @Test
+    public void testReadingsRangeNormal() {
+        Map<String, Integer> result;
+        result = BucketSampleApplication.getReadingsRange(new int[]{1,2,3,4});
+        assertSame(result.size(), 1);
+        assertTrue(result.containsKey("1-4"));
+        assertTrue(result.containsValue(4));
+
+        //unsorted array
+        result = BucketSampleApplication.getReadingsRange(new int[]{15,14,12,10,13,9,8,7,1,2,11,4,3,6,5});
+        assertSame(result.size(), 1);
+        assertTrue(result.containsKey("1-15"));
+        assertTrue(result.containsValue(15));
+
+        //multiple buckets
+        result = BucketSampleApplication.getReadingsRange(new int[]{20,8,1,2,9,21,4,5,6,20,22,21,22,9});
+        assertSame(result.size(), 4);
+        assertTrue(result.keySet().containsAll(Arrays.asList("1-2","20-22","8-9","4-6")));
+        assertSame(result.values().stream().mapToInt(Integer::intValue).sum(), 14);
+    }
+
+    @Test
+    public void testReadingsRangeCSV() {
+        String result = BucketSampleApplication.getReadingsRangeCSV(new int[]{4,5,6});
+        assertNotNull(result);
+        assertTrue(result.startsWith("Range, Readings"));
+        assertTrue(result.endsWith("\n"));
+        assertTrue(result.contains("4-6, 3"));
+
+        //multiple buckets
+        result = BucketSampleApplication.getReadingsRangeCSV(new int[]{20,8,1,2,9,21,4,5,6,20,22,21,22,9});
+        assertTrue(result.contains("1-2, 2\n4-6, 3\n8-9, 3\n20-22, 6\n"));
+    }
+
+    @Test
+    public void testReadingsForEmptyArray() {
+        Map<String, Integer> resultMap = BucketSampleApplication.getReadingsRange(new int[]{});
+        assertTrue(resultMap.isEmpty());
+        String resultStr = BucketSampleApplication.getReadingsRangeCSV(new int[]{});
+        assertTrue(resultStr.isEmpty());
+    }
+
+    @Test
+    public void testForSingleRangeValue() {
+        String resultStr = BucketSampleApplication.getReadingsRangeCSV(new int[]{5});
+        assertTrue(resultStr.contains("5, 1\n"));
+
+        Map<String, Integer> resultMap;
+        resultMap = BucketSampleApplication.getReadingsRange(new int[]{4,4,4,4});
+        assertSame(resultMap.size(), 1);
+        assertTrue(resultMap.containsKey("4"));
+        assertTrue(resultMap.containsValue(4));
+
+        //Single value range at start
+        resultMap = BucketSampleApplication.getReadingsRange(new int[]{3,8,9,6,7,8,8});
+        assertTrue(resultMap.keySet().containsAll(Arrays.asList("3","6-9")));
+        assertTrue(resultMap.values().contains(1));
+        assertTrue(resultMap.values().contains(6));
+
+        //Single value range at end
+        resultStr = BucketSampleApplication.getReadingsRangeCSV(new int[]{10,14,12,11,13,25,25});
+        assertTrue(resultStr.contains("25, 2\n"));
+        assertTrue(resultStr.contains("10-14, 5\n"));
+    }
+}
